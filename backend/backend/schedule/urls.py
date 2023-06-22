cd69608a4bf4d9b082de88e3d9cf29de16ff9951
eduper1 <eduper.havard@gmail.com>
2023-06-23 01:08:34 +0300

    Need to debug the API endpoint

--- backend/backend/schedule/urls.py
+++ backend/backend/schedule/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('api/todos/', views.todo_list, name='todo-list'),
 ]
--- backend/backend/schedule/views.py
+++ backend/backend/schedule/views.py
@@ -4,6 +4,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import TodoItem
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import TodoSerializer
 
 def index(request):
     return HttpResponse(TodoItem.objects.all())
+
+
+@api_view(['GET', 'POST'])
+def todo_list(request):
+    if request.method == 'GET':
+        todos = TodoItem.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
