0ceb874231f8328807c28e44246dc8f32a432b5b
eduper1 <eduper.havard@gmail.com>
2023-07-13 10:55:47 +0300

    Success and Alert messages validations added

--- frontend/schedule/src/componets/Forms.js
+++ frontend/schedule/src/componets/Forms.js
@@ -13,18 +13,33 @@ function Form(props){
     date:today,
   });
 
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
   function handleSubmit(e){
     e.preventDefault();
+
+    // Check if any field is empty
+    if (!FormData.title || !FormData.description || !FormData.date) {
+      alert("Please fill in all fields.");
+      return;
+    }
+    
     console.log("Submitted.")
     props.addSchedule(FormData);
-
+    
     setFormData({
-    title: '',
-    description: '',
-    date: today,
-  });
+      title: '',
+      description: '',
+      date: today,
+    });
+    setIsFormSubmitted(true);
+    // Reset success message after 2 seconds
+    setTimeout(() => {
+      setIsFormSubmitted(false);
+    }, 1500);
+    
   }
-
+  
   function handleInputChanges(e){
     const{name, value} = e.target;
     setFormData((prevData) => ({
@@ -41,18 +56,23 @@ function Form(props){
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">Add Task</h5>
-              <form onSubmit={handleSubmit}>
+              <form onSubmit={handleSubmit} className={isFormSubmitted ? "was-validated" : ""}>
+        {isFormSubmitted && (
+          <div className="alert alert-success" role="alert">
+            Form submitted successfully!
+          </div>
+        )}
                 <div className="form-group">
-                  <label htmlFor="title">Title:</label>
-                  <input type="text" name="title" value={FormData.title} className="form-control" id="title" onChange={handleInputChanges} />
+                  <label htmlFor="title" >Title:</label>
+                  <input type="text" name="title" value={FormData.title} className='form-control' id="title" onChange={handleInputChanges} />
                 </div>
                 <div className="form-group">
-                  <label htmlFor="description">Description:</label>
-                  <textarea className="form-control" name="description" value={FormData.description} id="description" rows="3"onChange={handleInputChanges} ></textarea>
+                  <label htmlFor="description" >Description:</label>
+                  <textarea className='form-control' name="description" value={FormData.description} id="description" rows="3"onChange={handleInputChanges} ></textarea>
                 </div>
                 <div className="form-group">
                   <label htmlFor="dueDate">Due Date:</label>
-                  <input type="date" name="date" value={FormData.date} className="form-control" id="dueDate" min={today} max={maxDateString} onChange={handleInputChanges} />
+                  <input type="date" name="date" value={FormData.date} className='form-control' id="dueDate" min={today} max={maxDateString} onChange={handleInputChanges} />
                 </div>
                 <button type="submit" className="btn btn-primary" >Submit</button>
               </form>
