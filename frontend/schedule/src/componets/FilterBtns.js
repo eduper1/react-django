255354e9fcba4b14723403df26b80bbc119eac89
eduper1 <eduper.havard@gmail.com>
2023-07-13 11:28:50 +0300

    Recentered Filter btns

--- frontend/schedule/src/componets/FilterBtns.js
+++ frontend/schedule/src/componets/FilterBtns.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 function FilterBtn() {
   return (
-    <div className="btn-group" role="group" aria-label="Todo Status">
-      <button type="button" className="btn btn-primary">Active</button>
-      <button type="button" className="btn btn-success">Complete</button>
-      <button type="button" className="btn btn-danger">Due past</button>
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className="btn-group" role="group" aria-label="Todo Status">
+          <button type="button" className="btn btn-primary">Active</button>
+          <button type="button" className="btn btn-success">Complete</button>
+          <button type="button" className="btn btn-danger">Due past</button>
+        </div>
+      </div>
     </div>
   );
 }
