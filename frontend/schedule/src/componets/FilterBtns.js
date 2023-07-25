3e2fbb6332ca6955da5313b4ca4850744f5aa5ae
eduper1 <eduper.havard@gmail.com>
2023-07-25 23:28:51 +0300

    Props: use props to reference to isDisplayed state

--- frontend/schedule/src/componets/FilterBtns.js
+++ frontend/schedule/src/componets/FilterBtns.js
@@ -1,21 +1,17 @@
 import React, { useState } from 'react';
 
-function FilterBtn() {
-  const [isDisplayed, setIsDisplayed] = useState('active');
+function FilterBtn(props) {
 
   return (
     <div className='container'>
       <div className='row justify-content-center'>
         <div className="btn-group mb-3 mt-3" role="group" aria-label="Todo Status">
-          <button type="button" className={`btn btn-primary ${isDisplayed === 'active' ? 'active':''}`} onClick={(active) => setIsDisplayed('active')}>Active</button>
-          <button type="button" className={`btn btn-success ${isDisplayed === 'complete' ? 'complete':''}`}onClick={(complete) => setIsDisplayed('complete')}>Complete</button>
-          <button type="button" className={`btn btn-danger ${isDisplayed === 'duePast' ? 'duePast':''}`}onClick={(duePast) => setIsDisplayed('duePast')}>Due past</button>
+          <button type="button" className={`btn btn-primary ${props.isDisplayed === 'active' ? 'active':''}`} onClick={(active) => props.setIsDisplayed('active')}>Active</button>
+          <button type="button" className={`btn btn-success ${props.isDisplayed === 'complete' ? 'complete':''}`}onClick={(complete) => props.setIsDisplayed('complete')}>Complete</button>
+          <button type="button" className={`btn btn-danger ${props.isDisplayed === 'duePast' ? 'duePast':''}`}onClick={(duePast) => props.setIsDisplayed('duePast')}>Due past</button>
         </div>
       </div>
-      {/* Display linked information based on activeButton */}
-      {isDisplayed === 'active' && <div>Active Todos</div>}
-      {isDisplayed === 'complete' && <div>Complete Todos</div>}
-      {isDisplayed === 'duePast' && <div>Due Past Todos</div>}
+      
     </div>
   );
 }
