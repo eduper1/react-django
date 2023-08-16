b198778f72285b7083aaef6df78bcc2f9b74ce8a
eduper1 <eduper.havard@gmail.com>
2023-08-16 17:15:05 +0300

    API: isComplete work properly

--- frontend/schedule/src/componets/FilterBtns.js
+++ frontend/schedule/src/componets/FilterBtns.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function FilterBtn(props) {
-
   return (
     <div className='container'>
       <div className='row justify-content-center'>
--- frontend/schedule/src/componets/Tod.js
+++ frontend/schedule/src/componets/Tod.js
@@ -12,7 +12,16 @@ function Todo(props){
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.description}</p>
             <p className="card-text">Due Date: {props.dueDate}</p>
-            <button className="btn btn-primary" disabled={props.isCompleted} onClick={()=> props.toggleCompleteBtn(props.id)} >{!props.isComplete ? "Progress":"Completed"}</button>
+            {!props.isCompleted && props.dueDate < props.currentDate ? (
+              <div className="d-flex justify-content-between" role="group" aria-label="scheduleStatus">
+                <button className="btn btn-warning" onClick={() => props.handleReschedule(props.id)}>Reschedule</button>
+                <button className="btn btn-danger" onClick={() => props.handleDelete(props.id)}>Delete</button>
+              </div>
+            ):(
+              <button className={!props.isComplete ? "btn btn-primary":"btn btn-success"} disabled={props.isComplete} onClick={()=> props.toggleCompleteBtn(props.id)} >{!props.isComplete ? "Complete":"Completed"}</button>
+            )}
+
+
           </div>
         </div>
           {/* </div> */}
