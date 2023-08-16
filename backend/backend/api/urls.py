1b401a33361397735f0be39db8f626f1a150cdec
eduper1 <eduper.havard@gmail.com>
2023-08-16 18:43:42 +0300

    Delete API: api route work properly

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.todo_list, name="todo_list"),
     path('<int:schedule_id>', views.update_schedule, name='update_schedule'),
+    path('delete/<int:id>', views.delete_schedule, name='delete_schedule'),
 ]
