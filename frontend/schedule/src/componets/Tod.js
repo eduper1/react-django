686a6daffbf8f0476aa66599ed8302221ce22fda
eduper1 <eduper.havard@gmail.com>
2023-08-17 09:03:24 +0300

    Reschedule: date form pops up good

--- frontend/schedule/src/componets/Tod.js
+++ frontend/schedule/src/componets/Tod.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Todo(props){
+  const today = new Date().toISOString().slice(0, 10);
+  const maxDate = new Date();
+  maxDate.setDate(maxDate.getDate() + 10); // Add 10 days to the current date
+  const maxDateString = maxDate.toISOString().slice(0, 10);
+
+
+  const [showRescheduleForm, setShowRescheduleForm] = useState(false);
+  const [newDueDate, setNewDueDate] = useState('');
+
+  const handleRescheduleDate = () => {
+    // Display the reschedule form when the Reschedule button is clicked
+    setShowRescheduleForm(true);
+  };
+
+  const handleInputChanges = (e) => {
+    // Update the newDueDate state when the input field changes
+    setNewDueDate(e.target.value);
+  };
+
+  const handleRescheduleSubmit = (e) => {
+    e.preventDefault();
+    // Call the props function to handle rescheduling with the newDueDate
+    props.handleReschedule(props.id, newDueDate);
+    // Hide the reschedule form after submitting
+    setShowRescheduleForm(false);
+  };
+  
     return(
         <div className="container">
           <div className="row justify-content-center">
@@ -14,14 +41,22 @@ function Todo(props){
             <p className="card-text">Due Date: {props.dueDate}</p>
             {!props.isCompleted && props.dueDate < props.currentDate ? (
               <div className="d-flex justify-content-between" role="group" aria-label="scheduleStatus">
-                <button className="btn btn-warning" onClick={() => props.handleReschedule(props.id)}>Reschedule</button>
+                <button className="btn btn-warning" onClick={() => handleRescheduleDate()}>Reschedule</button>
                 <button className="btn btn-danger" onClick={() => props.handleDelete(props.id)}>Delete</button>
               </div>
             ):(
               <button className={!props.isComplete ? "btn btn-primary":"btn btn-success"} disabled={props.isComplete} onClick={()=> props.toggleCompleteBtn(props.id)} >{!props.isComplete ? "Complete":"Completed"}</button>
             )}
 
-
+            {showRescheduleForm && (
+              <form onSubmit={handleRescheduleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="dueDate">Due Date:</label>
+                  <input type="date" name="date" value={newDueDate} className="form-control" id="dueDate" min={today} max={maxDateString} onChange={handleInputChanges} />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            )}
           </div>
         </div>
           {/* </div> */}
