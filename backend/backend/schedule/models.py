e872c8fa6bfc70918a395a6c63fd888990ac07df
eduper1 <eduper.havard@gmail.com>
2023-08-17 12:21:00 +0300

    Count reschedule: count how many times an event was rescheduled

--- backend/backend/schedule/models.py
+++ backend/backend/schedule/models.py
@@ -8,6 +8,10 @@ class TodoItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     dueDate = models.DateField(blank=False)
     isCompleted = models.BooleanField(default=False)
-
+    reschedule_count = models.PositiveIntegerField(default=0)
     def __str__(self):
-        return f"{self.title}{self.description}{self.created_at}{self.dueDate}{self.isCompleted}"
+        return f"{self.title}{self.reschedule_count}{self.description}{self.created_at}{self.dueDate}{self.isCompleted}"
+
+    def reschedule(self):
+        self.reschedule_count += 1
+        self.save()
